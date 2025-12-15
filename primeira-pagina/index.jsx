f673352edddd52c993c388bcc7d31900bc07b6f3
Kaiobrl <ikicicvt@gmail.com>
2025-12-15 13:49:54 -0300

    recomendações JSX

--- primeira-pagina/index.jsx
+++ primeira-pagina/index.jsx
@@ -1,15 +1,30 @@
-const element = <h1>Ola, mundo!</h1>
+// const element = <h1>Ola, mundo!</h1>
 
-const anotherElement = (
-    <div>
-        <h1>Texto</h1>
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <h2></h2>
-    </div>
-)
+// const anotherElement = (
+//     <div>
+//         <h1>Texto</h1>
+//         <ul>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//         </ul>
+//         <h2></h2>
+//     </div>
+// )
+
+//
+
+function PrimaryButton() {
+    return <button>Meu botão</button>
+}
+
+function Container() {
+    return (
+        <div className="">
+            <h1></h1>
+            <p></p>
+        </div>
+    )
+}
