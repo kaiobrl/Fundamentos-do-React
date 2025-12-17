e7e331386cb0422a9d2c28362b6630a1ce3ee361
Kaiobrl <ikicicvt@gmail.com>
2025-12-17 19:52:26 -0300

    Title index

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,16 +1,15 @@
-function sum(a, b) {
-  return a + b
-}
+import StatusText from "./components/StatusText.jsx"
+import { Subtitle } from "./components/Subtitle.jsx"
+import Title from "./components/Title.jsx"
 
+// PascalCase camelCase
 export default function App() {
-  const tech = "React"
-  const status = true
 
   return (
     <div>
-      <h1>{tech} is awesome!</h1>
-      <h2>It's easy, like 1 + 1 is {sum(1, 1)}</h2>
-      <h2>Current status: {status ? "ON" : "OFF"}</h2>
+      <Title />
+      <Subtitle />
+      <StatusText />
     </div>
   )
 }
