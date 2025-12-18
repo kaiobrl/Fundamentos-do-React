f7579eec09198a21bdfa176d60c9819aae5868bb
Kaiobrl <ikicicvt@gmail.com>
2025-12-18 11:12:46 -0300

    React | Estilizando Componentes

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,12 +1,13 @@
 import StatusText from "./components/StatusText.jsx"
 import { Subtitle } from "./components/Subtitle.jsx"
 import Title from "./components/Title.jsx"
+import styles from "./App.module.css"
 
 // PascalCase camelCase
 export default function App() {
 
   return (
-    <div>
+    <div className={styles.app}>
       <Title />
       <Subtitle />
       <StatusText />
--- react-fundamentals/src/components/StatusText.jsx
+++ react-fundamentals/src/components/StatusText.jsx
@@ -1,6 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 export default () => {
-    const status = true
-    
-    return <h2>Current status: {status ? "ON" : "OFF"}</h2>
+    const status = false
+
+    return (
+        <h2
+            style={{
+                color: status ? "#4ade80" : "#f87171"
+            }}
+        >
+            Current status: {status ? "ON" : "OFF"}
+        </h2>
+    )
 }
