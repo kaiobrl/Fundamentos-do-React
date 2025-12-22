0ed729bd4264bddc38debe21eba1c5cf2ac7e039
Kaiobrl <ikicicvt@gmail.com>
2025-12-22 12:39:44 -0300

    O que são e como usar as “props”

--- react-exercicio-2/src/App.jsx
+++ react-exercicio-2/src/App.jsx
@@ -1,10 +1,16 @@
 import Card from "./components/Card"
+import swPosterImg from "./assets/sw-poster.jpg"
+import esbPosterImg from "./assets/esb-poster.jpg"
+import rotjPosterImg from "./assets/rotj-poster.jpg"
+
 
 function App() {
   return (
     <>
       <h1>Exercicio 2</h1>
-      <Card />
+      <Card title="Pôster: Star Wars (1977)" posterImg={swPosterImg} />
+      <Card title="Pôster: Empire Strikes Back (1980)" posterImg={esbPosterImg} />
+      <Card title="Pôster: Return of the Jedi (1983)" posterImg={rotjPosterImg} />
     </>
   )
 }
--- react-exercicio-2/src/components/Card/index.jsx
+++ react-exercicio-2/src/components/Card/index.jsx
@@ -1,14 +1,14 @@
-import posterImg from "../../assets/sw-poster.jpg"
+import Button from "../Button"
 import styles from './styles.module.css'
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className={styles.container}>
-            <img className={styles.poster} src={posterImg} alt="Star Wars poster" />
+            <img className={styles.poster} src={props.posterImg} alt={props.title} />
             <div>
-                <h2>Pôster do filme Star Wars</h2>
+                <h2>{props.title}</h2>
                 <p>Um poster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico poster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
-                <button>Comprar agora</button>
+                <Button /> 
             </div>
         </div>
     )
