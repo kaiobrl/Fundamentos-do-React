d0ae6b715d320853400b27827523075aaf73e272
Kaiobrl <ikicicvt@gmail.com>
2025-12-23 11:19:45 -0300

    Exercício 3 terminou

--- react-exercicio-3/src/App.jsx
+++ react-exercicio-3/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Profile from "./components/Profile";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Profile
+        avatar="https://randomuser.me/api/portraits/men/75.jpg"
+        name="John Doe"
+        bio="Full-stack Javascript Developer at Acme inc."
+        email="John.doe@example.com"
+        phone="+5583981374944"
+        githubUrl="https://github.com/johndoe"
+        linkedinUrl="https://linkedin.com/in/johndoe"
+        twitterUrl="https://twitter.com/johndoe"
+      />
+    </div>
+  );
 }
-
-export default App
