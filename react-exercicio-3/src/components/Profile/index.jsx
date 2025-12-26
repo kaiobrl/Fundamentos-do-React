3d38ab546914e2b85adfadc25838e37ddc1e309d
Kaiobrl <ikicicvt@gmail.com>
2025-12-26 14:08:00 -0300

    React | Props com Rest Param

--- react-exercicio-3/src/components/Profile/index.jsx
+++ react-exercicio-3/src/components/Profile/index.jsx
@@ -14,12 +14,15 @@ export default function Profile(props) {
             <ProfileSection>{props.bio}</ProfileSection>
             <ProfileSection>{props.phone}</ProfileSection>
             <ProfileSection>{props.email}</ProfileSection>
-            <ProfileSection>
-                <div className={styles.links}>
-                    <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
-                    <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
-                    <LinkButton href={props.twitterUrl} target="_blank">Twitter</LinkButton>
-                </div>
+            <ProfileSection
+                className={styles.links}
+                id='links-section'
+                data-test='some value'
+                aria-label='social links'
+            >
+                <LinkButton href={props.githubUrl} target="_blank">GitHub</LinkButton>
+                <LinkButton href={props.linkedinUrl} target="_blank">LinkedIn</LinkButton>
+                <LinkButton href={props.twitterUrl} target="_blank">Twitter</LinkButton>
             </ProfileSection>
         </div>
     );
