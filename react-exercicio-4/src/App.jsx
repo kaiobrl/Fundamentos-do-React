5a9acd96200bcaf729f571e413d2275e5b04634b
Kaiobrl <ikicicvt@gmail.com>
2026-01-03 11:23:38 -0300

    Fundamentos do React | Resolução do Exercício 4

---
+++ react-exercicio-4/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+  <div className="app">
+    <h1>Gerador de senhas</h1>
+    <button>Gerar!</button>
+    <button>Copiar</button>
+    <button>senha</button>
+
+  </div>
+  )
+}
+
+export default App
