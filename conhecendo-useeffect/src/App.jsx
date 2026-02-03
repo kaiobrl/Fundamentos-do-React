ec1edb38448447909066427350d643aadaabef67
kaiobrl <ikicicvt@gmail.com>
2026-02-03 19:47:03 -0300

    React | Ciclo de vida: conhecendo o useEffect

--- conhecendo-useeffect/src/App.jsx
+++ conhecendo-useeffect/src/App.jsx
@@ -1,20 +1,40 @@
 import { useEffect, useState } from "react";
 
-export default function App() {
+function Counter() {
   const [counter, setCounter] = useState(0);
-  
+
   useEffect(() => {
-    alert("O efeito colateral foi ativado!")
-  }, [counter])
+    alert("chamando a api");
+
+    return () => {
+      alert("chamanda cancelada");
+    };
+  }, []);
+
+  return (
+    <button onClick={() => setCounter((count) => count + 1)}>
+      Contador: {counter}
+    </button>
+  );
+}
+
+export default function App() {
+  const [show, setShow] = useState(false);
 
   return (
     <>
       <h1>Conhecendo o useEffect</h1>
-      <button
-        onClick={() => setCounter((count) => count + 1)}
-       >
-        Contador: {counter}
-      </button>
+      <div>
+        <input
+          type="checkbox"
+          id="show"
+          value={show}
+          onChange={() => setShow((state) => !state)}
+        />
+        <label htmlFor="show">Exibir</label>
+      </div>
+      {show ? <Counter /> : null}
+      <hr />
     </>
   );
 }
