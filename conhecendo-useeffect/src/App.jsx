7ac87dbcf7d4bd23b25dcb72997ad937f0948925
kaiobrl <ikicicvt@gmail.com>
2026-02-05 19:38:16 -0300

    Requisições HTTP com useEffect

--- conhecendo-useeffect/src/App.jsx
+++ conhecendo-useeffect/src/App.jsx
@@ -1,40 +1,35 @@
 import { useEffect, useState } from "react";
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("chamando a api");
-
-    return () => {
-      alert("chamanda cancelada");
-    };
-  }, []);
-
-  return (
-    <button onClick={() => setCounter((count) => count + 1)}>
-      Contador: {counter}
-    </button>
-  );
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const data = await response.json();
+  return data.results;
 }
 
 export default function App() {
-  const [show, setShow] = useState(false);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetchPokemon().then((results) => {
+      console.log("Requisição realizada.");
+      console.log(results);
+      setPokemon(results);
+    });
+  }, []);
 
   return (
-    <>
-      <h1>Conhecendo o useEffect</h1>
+    <div className="app">
       <div>
-        <input
-          type="checkbox"
-          id="show"
-          value={show}
-          onChange={() => setShow((state) => !state)}
-        />
-        <label htmlFor="show">Exibir</label>
+        <h2>Pokémon</h2>
+        <ul className="pokemon">
+          {pokemon.map((mon) => (
+            <li key={mon.name}>
+              <span>{mon.name}</span>
+              <button>Ver detalhes</button>
+            </li>
+          ))}
+        </ul>
       </div>
-      {show ? <Counter /> : null}
-      <hr />
-    </>
+    </div>
   );
 }
