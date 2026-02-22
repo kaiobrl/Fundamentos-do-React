00e21100ac051201bd1b84048e5d38c591506d6e
kaiobrl <ikicicvt@gmail.com>
2026-02-21 21:20:51 -0300

    React | Lidando com Data Fetching e Erros

---
+++ trabalhando-com-spas/src/loaders/products.js
@@ -0,0 +1,11 @@
+import products from "../database.json";
+
+export default function loadProduct({ params }) {
+  const product = products.find((p) => p.id === +params.productId);
+
+  if (!product) {
+    throw new Response("404 Not found", { status: 404 });
+  }
+
+  return product;
+}
--- trabalhando-com-spas/src/pages/Product.jsx
+++ trabalhando-com-spas/src/pages/Product.jsx
@@ -1,16 +1,7 @@
-import { Link, useParams } from "react-router-dom";
-import products from "../database.json";
+import { Link, useLoaderData } from "react-router-dom";
 
 export default function Product() {
-  const { productId } = useParams();
-
-  const product = products.find((p) => p.id === +productId);
-
-  if (!product) {
-    return (
-        <h2>Oops... Esse produto não foi encontrado =(</h2>
-    );
-  }
+  const product = useLoaderData();
 
   return (
     <section>
