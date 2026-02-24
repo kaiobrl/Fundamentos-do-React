79414b37f871e1cd4215ee6f8171770ea350f93c
kaiobrl <ikicicvt@gmail.com>
2026-02-24 20:19:26 -0300

    Resolução do Exercício Final - Parte 1

--- react-stock/src/App.jsx
+++ react-stock/src/App.jsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
 export default function App() {
   return (
     <>
-    <h1>App</h1>
+    <RouterProvider router={router} />
     </>
   )
 }
