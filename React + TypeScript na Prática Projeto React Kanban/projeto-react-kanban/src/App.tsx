5e9ac1cd521f424f9ad08d405eb9580c3345984d
kaiobrl <ikicicvt@gmail.com>
2026-03-19 13:37:23 -0300

    Criação da Interface - Parte 2

--- React + TypeScript na Prática Projeto React Kanban/projeto-react-kanban/src/App.tsx
+++ React + TypeScript na Prática Projeto React Kanban/projeto-react-kanban/src/App.tsx
@@ -1,10 +1,20 @@
-import { Box, Heading } from "@radix-ui/themes"
+import { Box, Flex, Heading } from "@radix-ui/themes"
+import { CreateTaskForm } from "./components/CreateTaskForm"
 
 
 function App() {
   return (
-    <Box>
-    <Heading>React Kanban</Heading>
+    <Box maxWidth="80rem" mx="auto">
+      <Box height="4rem">
+        <Flex align="center" gap="4" height="100%">
+          <Heading as="h1" size="8" weight="light">React Kanban</Heading>
+          <CreateTaskForm />
+        </Flex>
+      </Box>
+
+      <Box>
+        <Heading as="h2">Quadro de tarefas</Heading>
+      </Box>
     </Box>
   )
 }
